71740a9490f1959003def7d62f8c1773eb9033a1
bentalla108 <tallambaye108@gmail.com>
2023-01-02 23:02:28 +0000

    Docstring check

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" Rectangle Module """
+""" Rectangle module"""
 
 
 class Rectangle:
-    """ Rectangle Class"""
+    """ Rectangle class"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -11,7 +11,7 @@ class Rectangle:
     @property
     def width(self):
         """Returns the width of the Rectangle"""
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
@@ -25,7 +25,7 @@ class Rectangle:
     @property
     def height(self):
         """Returns the width of the Rectangle"""
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
