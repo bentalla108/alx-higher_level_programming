df67cf393a7a2c0553f0b945e52d1c1be7fdd72f
bentalla108 <tallambaye108@gmail.com>
2023-01-02 16:15:20 +0000

    NOk for push 1-rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    """ Rectangle Class"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retunr the width of the Rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle """
+        if not isinstance(value, int):
+             raise ValueError("width must be an integer")
+
+        if value < 0 :
+            raise ValueError("width must be >= 0")
+        
+        self.__width = value 
+
+    @property
+    def height(self):
+        """Retunr the width of the Rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle """
+        if not isinstance(value, int):
+            raise ValueError("height must be an integer")
+
+        if value < 0 :
+            raise ValueError("height must be >= 0")
+        
+        self.__height = value 
