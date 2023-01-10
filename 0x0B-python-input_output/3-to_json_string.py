e6488fe40650134ae53b577e25640c954ff5d471
bentalla108 <tallambaye108@gmail.com>
2023-01-10 23:22:36 +0000

    unction that returns the JSON representation of an object (string)

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,5 +2,7 @@
 """Module returns the JSON representation """
 import json
 
+
 def to_json_string(my_obj):
+    """Method that returns the JSON representation of an object"""
     return json.dumps(my_obj)
