892e46ce5ae06eee13fa0a37a50c9d22a2582d6c
bentalla108 <tallambaye108@gmail.com>
2023-01-10 22:22:19 +0000

    Fonction that read a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that read a file """
+
+
+def read_file(filename=""):
+    """ Read file fonction"""
+    with open(filename, 'r', encoding="utf-8") as file:
+        file_data = file.read()
+        file.close()
