e5bf64ea56602ad75e5b42b5f1d7de40ef6365b6
bentalla108 <tallambaye108@gmail.com>
2023-01-10 23:27:00 +0000

    ith from_json_string method

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module returns thejson_string method"""
+import json
+
+def from_json_string(my_str):
+    """Method that returns an Object from JSON string"""
+    return json.loads(my_str)
