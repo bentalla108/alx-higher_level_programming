26d25b74f4a4159ff390c85aa1156c602ce639d0
bentalla108 <tallambaye108@gmail.com>
2023-02-15 11:55:12 -0800

    Write a script that creates the database hbtn_0d_2 and the user user_0d_2.

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,9 +3,9 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Create user
-CREATE USER IF NOT EXISTS "user_0d_2"@"localhost"  IDENTIFIED BY "user_0d_1_pwd" ;
+CREATE USER IF NOT EXISTS "user_0d_2"@"localhost"  IDENTIFIED BY "user_0d_2_pwd" ;
 
 -- Privileges 
 
-GRANT SELECT ON hbtn_0d_2 TO user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
 FLUSH PRIVILEGES;
