dcc27503404dff73330d50babc51e4f3124110a2
bentalla108 <tallambaye108@gmail.com>
2023-03-13 23:35:57 +0000

    Is integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const value = process.argv[2];
-if (Number.isInteger(Number.parseInt(value))) {
+const value = process.argv;
+if (Number.isInteger(Number.parseInt(value[2]))) {
   console.log(`My number: ${value[2]}`);
 } else {
-  console.log('Not a number ');
+  console.log('Not a number');
 }
