b3f94dd478df8704c690d118d997b21a6d71196c
bentalla108 <tallambaye108@gmail.com>
2023-03-19 17:59:17 +0000

    Changing testing port to 3306

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,20 +4,12 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-
-    if len(argv) < 4 :
-        db = MySQLdb.connect(
+    db = MySQLdb.connect(
             host='localhost',
-            port=3333,
+            port=3306,
             user=argv[1],
-            db=argv[2])
-    else : 
-        db = MySQLdb.connect(
-                host='localhost',
-                port=3333,
-                user=argv[1],
-                passwd=argv[2],
-                db=argv[3])
+            passwd=argv[2],
+            db=argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cursor.fetchall()
