48a53e4d39c175215a9dc9298156955365a1a928
bentalla108 <tallambaye108@gmail.com>
2023-03-19 18:17:34 +0000

    updating' for row in rows:'

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
                     states.id ASC""",)
     rows = cursor.fetchall()
     for row in rows:
-        print(row)
+        if row[1].startswith("N"):
+            print(row)
     cursor.close()
     db.close()
