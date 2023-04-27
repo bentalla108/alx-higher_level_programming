c5c20d33f8400b810b5ad76c98455d596fe254c6
bentalla108 <tallambaye108@gmail.com>
2023-04-27 03:12:47 +0000

    DIND FIN

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,23 +1,20 @@
-#!/usr/bin/node
-
-const r = require('request');
+const request = require('request');
 const fs = require('fs');
-const url = process.argv[2];
-const file = process.argv[3];
-let content;
-r(url, (err, reponse, body) => {
-  if (err) {
-    console.error(err);
-  }
-  const resultquery = reponse.statusCode === 200 ? body : null;
-  if (!resultquery) {
-    console.log(resultquery);
-  }
-  content = resultquery;
 
-  fs.writeFile(file, content, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    }
-  });
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(filePath, body, 'utf8', err => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`The contents of ${url} have been saved to ${filePath}.`);
+      }
+    });
+  }
 });
+
