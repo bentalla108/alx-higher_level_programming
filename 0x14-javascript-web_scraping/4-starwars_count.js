ddf5f80ca65a5faacbf9f6f208cb303bb6654b62
bentalla108 <tallambaye108@gmail.com>
2023-04-27 01:48:10 +0000

    LoveCodeWithTernaireExpression ? "yes":"yes"

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,13 +2,14 @@
 
 const r = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/';
-const id18 = 'https://swapi-api.alx-tools.com/api/people/18/';
+const id = 18;
 
 r(url, (err, reponse, body) => {
   if (err) {
     console.error(err);
   }
   const resultquery = reponse.statusCode === 200 ? JSON.parse(body).results : null;
-  const count = resultquery ? resultquery.filter(element => element.characters.includes(id18)).length : 0;
+  const count = resultquery ? resultquery.filter(element => element.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)).length : 0;
   console.log(resultquery ? count : 'error code: ' + reponse.statusCode);
 });
+
