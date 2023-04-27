3696784521228ab51aa0dfa38729a42917205c51
bentalla108 <tallambaye108@gmail.com>
2023-04-27 01:59:30 +0000

    Ternaire is not for all task

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,13 +2,21 @@
 
 const r = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/';
-const id = 18;
+const id = '18';
 
 r(url, (err, reponse, body) => {
   if (err) {
     console.error(err);
   }
   const resultquery = reponse.statusCode === 200 ? JSON.parse(body).results : null;
-  const count = resultquery ? resultquery.filter(element => element.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)).length : 0;
+  let count = 0;
+  if (resultquery) {
+    resultquery.forEach(element => {
+      if (element.characters.includes(id)) {
+        count++;
+      }
+    });
+  }
   console.log(resultquery ? count : 'error code: ' + reponse.statusCode);
 });
+
